250db4fa8c33cd488b7b4b95ceb4ab0c6d3be441
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-05-26 17:58:53 +0300

    Edit button

--- front/components/Button/Button.jsx
+++ front/components/Button/Button.jsx
@@ -1,22 +1,32 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { usePageTransition } from "../../hooks/usePageTransition";
 import { useParams } from "next/navigation";
 import styles from "./Button.module.css";
 
-const Button = ({ title, href, className }) => {
+const Button = ({ title, href = "", isBack = false, className }) => {
   const { locale } = useParams();
   const animateTransition = usePageTransition();
 
   return (
     <Link
       href={`/${locale}${href}`}
-      className={`${className} ${styles.button}`}
+      className={className}
       onClick={(e) => {
         e.preventDefault();
-        document.body.classList.add("bodyHidden");
+        // document.body.classList.add("bodyHidden");
         // window.history.replaceState({ customState: true }, "", "/projects");
-        animateTransition(`/${locale}${href}`);
+        // animateTransition(`/${locale}${href}`);
+        if (isBack) {
+          animateTransition(`/${locale}${href}`);
+          setTimeout(() => {
+            document.body.classList.remove("bodyHidden");
+          }, 3000);
+        } else {
+          document.body.classList.add("bodyHidden");
+          animateTransition(`/${locale}${href}`);
+        }
       }}
     >
       {title}
