ada4dcabcfd1764658f06f7462d92bceea2f79fc
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-05-26 17:55:13 +0300

    Add Image config in Next config

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     viewTransition: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost", // заміни на реальний домен з STRAPI_BASE_URL
+        pathname: "/uploads/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co", // заміни на реальний домен з STRAPI_BASE_URL
+      },
+    ],
+  },
 };
 
 export default nextConfig;
