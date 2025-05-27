a7a2f6dbe94a385aa102661f5a58dd46567146db
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-05-27 20:34:53 +0300

    Create ProjecDesktopView component

---
+++ front/components/ProjectDesktopView/ProjectDesktopView.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import st from "./ProjectDesktopView.module.css";
+import Markdown from "react-markdown";
+import Button from "../Button/Button";
+import Carousel from "../Carousel/Carousel";
+
+const ProjectDesktopView = ({ project }) => {
+  return (
+    <>
+      <Carousel images={project.images} />
+      {/* <Image src={project.url} alt={project.title} fill sizes="100%"></Image> */}
+      <div className={st.container}>
+        <div className={st.contentWrapper}>
+          <header className={st.header}>
+            <Button className={st.button} title="Label" isBack />
+            <Button className={st.button} title="Назад" href="/projects" />
+          </header>
+          <h1 className={st.title}>{project.title}</h1>
+          <p>{project.content}</p>
+          {project.paragraphs.map((paragraph) => (
+            <Markdown
+              key={paragraph.paragraphs}
+              components={{
+                ul: ({ children }) => <ul className={st.list}>{children}</ul>,
+                li: ({ children }) => (
+                  <li className={st.listItem}>{children}</li>
+                ),
+                p: ({ children }) => <p className={st.paragraph}>{children}</p>,
+              }}
+            >
+              {paragraph.paragraphs}
+            </Markdown>
+          ))}
+          <p>{project.year}</p>
+          <p>{project.customer}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProjectDesktopView;
