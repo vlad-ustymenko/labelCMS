fa4ec2c132f5906c706125b3419b0e9ea5abafa8
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-05-27 20:34:20 +0300

    Create ProjectPage

---
+++ front/components/ProjectPage/ProjectPage.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import ProjectMobileView from "../ProjectMobileView/ProjectMobileView";
+import ProjectDesktopView from "../ProjectDesktopView/ProjectDesktopView";
+import Container from "../Container/Container";
+
+const ProjectPage = ({ project }) => {
+  const isMobile = window.innerWidth < 768;
+
+  return isMobile ? (
+    <ProjectMobileView project={project} />
+  ) : (
+    <ProjectDesktopView project={project} />
+  );
+};
+
+export default ProjectPage;
