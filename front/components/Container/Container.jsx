92a9be13de39f4cdb0492adba776bb81bb5aba8e
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-05-27 20:36:28 +0300

    Add Containe className child

--- front/components/Container/Container.jsx
+++ front/components/Container/Container.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import st from "./Container.module.css";
 
-const Container = ({ children }) => {
-  return <div className={st.container}>{children}</div>;
+const Container = ({ children, className }) => {
+  return <div className={`${st.container} ${className}`}>{children}</div>;
 };
 
 export default Container;
