6935b079eac0bf696d73b449fe259885c685912b
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-05-30 17:51:41 +0300

    Add videos on BG

--- front/components/Sofa/Sofa.jsx
+++ front/components/Sofa/Sofa.jsx
@@ -9,60 +9,60 @@ import { useRef, useState } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styles from "./Sofa.module.css";
 
-function Model({ isMobile }) {
-  const gltf = useGLTF("/models/new.glb");
-  const ref = useRef();
-  gltf.scene.traverse((child) => {
-    if (child.isMesh) {
-      child.material.color.set("#333333");
-      // child.material.roughness = 0.9;
-    }
-  });
-  return (
-    <primitive
-      ref={ref}
-      object={gltf.scene}
-      position={[isMobile ? 0 : 0, isMobile ? -0.5 : 0, isMobile ? -1 : 0]}
-    />
-  );
-}
+// function Model({ isMobile }) {
+//   const gltf = useGLTF("/models/new.glb");
+//   const ref = useRef();
+//   gltf.scene.traverse((child) => {
+//     if (child.isMesh) {
+//       child.material.color.set("#333333");
+//       // child.material.roughness = 0.9;
+//     }
+//   });
+//   return (
+//     <primitive
+//       ref={ref}
+//       object={gltf.scene}
+//       position={[isMobile ? 0 : 0, isMobile ? -0.5 : 0, isMobile ? -1 : 0]}
+//     />
+//   );
+// }
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-function CameraAnimation({ isMobile }) {
-  const { camera } = useThree();
+// function CameraAnimation({ isMobile }) {
+//   const { camera } = useThree();
 
-  const startPos = { x: 0, y: 1, z: 1.3 };
-  const endPos = { x: 0.5, y: 1, z: 1.3 };
+//   const startPos = { x: 0, y: 1, z: 1.3 };
+//   const endPos = { x: 0.5, y: 1, z: 1.3 };
 
-  useEffect(() => {
-    // Встановити початкову позицію
-    camera.position.set(startPos.x, startPos.y, startPos.z);
+//   useEffect(() => {
+//     // Встановити початкову позицію
+//     camera.position.set(startPos.x, startPos.y, startPos.z);
 
-    // Початкова анімація
-    gsap.to(camera.position, {
-      x: endPos.x,
-      y: endPos.y,
-      z: endPos.z,
-      duration: 2,
-      ease: "power2.out",
-    });
+//     // Початкова анімація
+//     gsap.to(camera.position, {
+//       x: endPos.x,
+//       y: endPos.y,
+//       z: endPos.z,
+//       duration: 2,
+//       ease: "power2.out",
+//     });
 
-    gsap.to(camera.position, {
-      z: isMobile ? 10 : 4,
-      ease: "none",
-      scrollTrigger: {
-        trigger: "body",
-        scroller: isMobile ? "body" : "[data-scroll-container]",
-        start: "top top",
-        end: "bottom bottom",
-        scrub: true,
-      },
-    });
-  }, [camera]);
+//     gsap.to(camera.position, {
+//       z: isMobile ? 10 : 4,
+//       ease: "none",
+//       scrollTrigger: {
+//         trigger: "body",
+//         scroller: isMobile ? "body" : "[data-scroll-container]",
+//         start: "top top",
+//         end: "bottom bottom",
+//         scrub: true,
+//       },
+//     });
+//   }, [camera]);
 
-  return null;
-}
+//   return null;
+// }
 
 export default function Sofa() {
   const isMobile = window.innerWidth < 768;
@@ -78,14 +78,21 @@ export default function Sofa() {
       }}
       className="ok"
     >
-      <Canvas camera={{ position: [0.5, 1, 1.3], fov: 50 }}>
+      <video
+        autoPlay
+        muted
+        playsInline
+        className={styles.video}
+        src="/video/Main.webm"
+      ></video>
+      {/* <Canvas camera={{ position: [0.5, 1, 1.3], fov: 50 }}>
         <CameraAnimation isMobile={isMobile} />
         <ambientLight intensity={0.3} />
         <directionalLight position={[1, 2, 3]} intensity={0.2} />
         <OrbitControls enableZoom={false} enableRotate={false} />
         <Environment preset="city" />
         <Model isMobile={isMobile} />
-      </Canvas>
+      </Canvas> */}
     </div>
   );
 }
