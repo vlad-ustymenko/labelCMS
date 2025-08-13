fa965bcf3108acacd61820b3f2f4741ff538d418
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-08-13 15:20:01 +0300

    Create repetable component for sections andd add in MainPage

--- front/app/[locale]/page.js
+++ front/app/[locale]/page.js
@@ -4,10 +4,10 @@ import Roadmap from "@/components/Roadmap/Roadmap";
 import Loader from "@/components/Loader/Loader";
 import qs from "qs";
 import { notFound } from "next/navigation";
-import Services from "@/components/Services/Services";
+import Sections from "@/components/Sections/Sections";
 import Footer from "@/components/Footer/Footer";
 import BgPhone from "@/components/BgPhone/BgPhone";
-
+import Achievements from "@/components/Achievements/Achievements";
 async function getData(path, locale) {
   const baseUrl = process.env.STRAPI_BASE_URL;
 
@@ -18,14 +18,11 @@ async function getData(path, locale) {
         on: {
           "blocks.main-screen": {
             populate: {
-              image: {
-                fields: ["url"],
-              },
-              button: {
-                fields: ["title", "href"],
-              },
               spinningText: {
                 fields: ["text"],
+              },
+              header: {
+                populate: "*",
               },
             },
           },
@@ -33,6 +30,9 @@ async function getData(path, locale) {
             populate: "*",
           },
           "blocks.services": {
+            populate: "*",
+          },
+          "blocks.achievements": {
             populate: "*",
           },
         },
@@ -50,6 +50,7 @@ async function getData(path, locale) {
 
     if (!res.ok) {
       console.error(`Strapi error: ${res.status} ${res.statusText}`);
+      return;
     }
 
     const data = await res.json();
@@ -59,14 +60,29 @@ async function getData(path, locale) {
   }
 }
 
+// function blockRendered(block) {
+//   switch (block.__component) {
+//     case "blocks.main-screen":
+//       return <MainScreen key={block.id} data={block} />;
+//     case "blocks.about":
+//       return <About key={block.id} data={block} />;
+//     case "blocks.services":
+//       return <Services key={block.id} data={block} />;
+//     default:
+//       return <Loader />;
+//   }
+// }
+
 function blockRendered(block) {
   switch (block.__component) {
     case "blocks.main-screen":
       return <MainScreen key={block.id} data={block} />;
     case "blocks.about":
-      return <About key={block.id} data={block} />;
+      return <Sections key={block.id} data={block} about={true} />;
     case "blocks.services":
-      return <Services key={block.id} data={block} />;
+      return <Sections key={block.id} data={block} services={true} />;
+    case "blocks.achievements":
+      return <Sections key={block.id} data={block} achievements={true} />;
     default:
       return <Loader />;
   }
@@ -85,6 +101,7 @@ export default async function Home({ params }) {
   return (
     <main>
       {blocks.map((block) => blockRendered(block))}
+      {/* <Achievements></Achievements> */}
       <BgPhone></BgPhone>
       <Roadmap />
       <Footer />
