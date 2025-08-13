4cf9d4ef9c611719aa658b6e7a08eb51b68b1036
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-08-13 15:24:36 +0300

    Menu: add data from CMS and bodyHidden when Menu active

--- front/components/Menu/Menu.jsx
+++ front/components/Menu/Menu.jsx
@@ -4,37 +4,50 @@ import { useMenuContext } from "@/context/MenuContext";
 import Link from "next/link";
 import { usePageTransition } from "../../hooks/usePageTransition";
 import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
 import st from "./Menu.module.css";
 
-const Menu = () => {
+const Menu = ({ data }) => {
   const { locale } = useParams();
   const animateTransition = usePageTransition();
   const { activeMenu, setActiveMenu } = useMenuContext();
+
+  const { projectsButton, projectsHref, businessButton, businessHref, phone } =
+    data;
+
+  useEffect(() => {
+    if (activeMenu) {
+      document.documentElement.classList.add("bodyHidden");
+    } else {
+      document.documentElement.classList.remove("bodyHidden");
+    }
+  }, [activeMenu]);
+
   return (
     <div className={`${st.menu} ${activeMenu ? st.active : ""}`}>
       <Link
-        href="/en/projects"
+        href="/projects"
         onClick={(e) => {
           e.preventDefault();
-          animateTransition(`/${locale}/projects`);
+          animateTransition(`/${locale}${projectsHref}`);
           setActiveMenu(false);
         }}
       >
-        Проекти
+        {projectsButton}
       </Link>
       <Link
         href="/business"
         onClick={(e) => {
           e.preventDefault();
-          animateTransition(`/${locale}/bisiness`);
+          animateTransition(`/${locale}${businessHref}`);
           setActiveMenu(false);
         }}
       >
-        Для бізнесу
+        {businessButton}
       </Link>
-      <Link className={st.phone} href="tel:+380660084031">
-        +380660084031
+      <Link className={st.phone} href={`tel:${phone}`}>
+        {phone}
       </Link>
     </div>
   );
