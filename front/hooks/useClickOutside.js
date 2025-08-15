694d47041e64cb9ac561f6ad4349ba3bbdea4132
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-08-15 18:11:22 +0300

    Create useClickOutside hook

---
+++ front/hooks/useClickOutside.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+const useClickOutside = (handler) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        handler();
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [handler]);
+
+  return ref;
+};
+
+export default useClickOutside;
