9e13b3afc8ce9c148b812121a2355e6c914ebd84
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-09-01 20:26:25 +0300

    Add i18n on BusinessPege in CMS

--- CMS/types/generated/contentTypes.d.ts
+++ CMS/types/generated/contentTypes.d.ts
@@ -399,7 +399,12 @@ export interface ApiBusinessBusiness extends Struct.SingleTypeSchema {
         'blocks.footer',
         'blocks.approaches',
       ]
-    >;
+    > &
+      Schema.Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
