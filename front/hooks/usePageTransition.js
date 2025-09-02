637012232c9d9a28f39f074658de93103bea7dff
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-09-02 16:30:49 +0300

    usePageTransition: add supportViewTransition

--- front/hooks/usePageTransition.js
+++ front/hooks/usePageTransition.js
@@ -1,36 +1,52 @@
+"use client";
+
 import { useTransitionRouter } from "next-view-transitions";
+import gsap from "gsap";
 
 export const usePageTransition = () => {
   const router = useTransitionRouter();
 
   const animateTransition = (url) => {
-    const animate = () => {
-      document.documentElement.animate(
-        [
-          { opacity: 1, transform: "translateY(0) scale(1)" },
-          { opacity: 0, transform: "translateY(-200px) scale(0.8)" },
-        ],
-        {
-          duration: 800,
-          easing: "cubic-bezier(0.76, 0, 0.24, 1)",
-          fill: "forwards",
-          pseudoElement: "::view-transition-old(root)",
-        }
-      );
-      document.documentElement.animate(
-        [
-          { opacity: 0, transform: "translateY(100%) scale(0.5)" },
-          { opacity: 1, transform: "translateY(0) scale(1)" },
-        ],
-        {
-          duration: 700,
-          easing: "cubic-bezier(0.76, 0, 0.24, 1)",
-          fill: "forwards",
-          pseudoElement: "::view-transition-new(root)",
-        }
-      );
-    };
-    router.push(url, { onTransitionReady: animate });
+    const supportsViewTransition =
+      typeof document !== "undefined" && "startViewTransition" in document;
+
+    console.log("supportsViewTransition:", supportsViewTransition);
+
+    if (supportsViewTransition) {
+      const animate = () => {
+        // стара сторінка виїжджає вгору
+        document.documentElement.animate(
+          [
+            { opacity: 1, transform: "translateY(0) scale(1)" },
+            { opacity: 0, transform: "translateY(-200px) scale(0.8)" },
+          ],
+          {
+            duration: 800,
+            easing: "cubic-bezier(0.76, 0, 0.24, 1)",
+            fill: "forwards",
+            pseudoElement: "::view-transition-old(root)",
+          }
+        );
+
+        // нова сторінка просто з’являється (якщо треба, можна прибрати)
+        document.documentElement.animate(
+          [
+            { opacity: 0, transform: "translateY(100%) scale(0.5)" },
+            { opacity: 1, transform: "translateY(0) scale(1)" },
+          ],
+          {
+            duration: 700,
+            easing: "cubic-bezier(0.76, 0, 0.24, 1)",
+            fill: "forwards",
+            pseudoElement: "::view-transition-new(root)",
+          }
+        );
+      };
+
+      router.push(url, { onTransitionReady: animate });
+    } else {
+      router.push(url);
+    }
   };
 
   return animateTransition;
