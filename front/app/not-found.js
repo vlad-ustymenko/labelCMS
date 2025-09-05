6f3cf903a9f3d2ceaae4c7ed7f310a1988c50506
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-09-05 19:03:31 +0300

    Edit notFound Page

--- front/app/not-found.js
+++ front/app/not-found.js
@@ -1,25 +1,42 @@
 "use client";
-import Link from "next/link";
-import { usePageTransition } from "../hooks/usePageTransition";
 import styles from "./not-found.module.css";
+import { usePathname } from "next/navigation";
+import Button from "@/components/Button/Button";
 import "../app/globals.css";
 
 export default function NotFound() {
-  const animateTransition = usePageTransition();
+  const pathname = usePathname();
+  const locale = pathname.split("/")[1];
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>sdcvwe</h1>
-      <p className={styles.text}>sdvsd</p>
-      <Link
-        href="/"
-        className={styles.link}
-        onClick={(e) => {
-          e.preventDefault();
-          animateTransition(`/`);
-        }}
-      >
-        wefwe
-      </Link>
+      <h1 className={styles.title}>
+        {locale === "en" ? "So sorry! " : "Вибачте!"}
+      </h1>
+      <h2 className={styles.subtitle}>
+        {locale === "en"
+          ? "The page you're looking for cannot be found"
+          : "Сторінку, яку ви шукаєте, не знайдено"}
+      </h2>
+      <p className={styles.textTitle}>
+        {locale === "en" ? "Possible reasons:" : "Можливі причини:"}
+      </p>
+      <p className={styles.text}>
+        {locale === "en"
+          ? "The link was entered incorrectly."
+          : "Посилання було введено неправильно."}
+      </p>
+      <p className={styles.text}>
+        {locale === "en"
+          ? "The link is broken or outdated."
+          : "Посилання не працює або є застарілим."}
+      </p>
+
+      <Button
+        title={locale === "en" ? "Main" : "Головна"}
+        primary
+        link
+        href={`/`}
+      />
     </main>
   );
 }
